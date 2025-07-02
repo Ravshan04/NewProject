7ccf789671fcac492a512ddb5a3acd624dc260e1
Ravshan04 <rn3925471@gmail.com>
2025-07-02 21:54:10 +0500

    first commit

---
+++ Ravshanboy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ravshanboy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
